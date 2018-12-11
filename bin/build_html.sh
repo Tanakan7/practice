59146a8c6d0537f89008ae0dd4bc45495b5808af
TANAKA KANJI <tanaka-kan@gnavi.co.jp>
2018-12-11 14:07:02 +0900

    ejsファイル単位での監視・コンパイル(廃止予定

--- bin/build_html.sh
+++ bin/build_html.sh
@@ -1,5 +1,17 @@
 echo "----- [build_html.sh] -----"
+echo input: $1
+
+# パスを消してファイル名のみ取得
+INPUT_FILE=$(basename $1)
+
 ejs-cli \
-  --base-dir src/ejs/html/ 'hoge.ejs' \
+  --base-dir src/ejs/html/ ${INPUT_FILE} \
   --out dist/ \
   --options src/ejs/data/bundle.json
+
+# 拡張子置換
+OUTPUT_FILE=${INPUT_FILE/.ejs/.html}
+
+js-beautify \
+  --replace "dist/${OUTPUT_FILE}" \
+  --indent-size 2
