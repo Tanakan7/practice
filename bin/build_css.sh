56fdcdf4b1d6e4157e8649820766a890c8453ba5
TANAKA KANJI <tanaka-kan@gnavi.co.jp>
2018-12-21 21:08:45 +0900

    postcss 処理追加

--- bin/build_css.sh
+++ bin/build_css.sh
@@ -1,41 +1,29 @@
 echo "----- [build_css.sh] -----"
 
-stylelint --config conf/stylelintrc.js src/scss/**/*.scss
+stylelint --config conf/stylelintrc.js src/scss/**/*.css --syntax scss
 
-node-sass src/scss/style.scss \
-  --output dist/css/ \
-  --output-style compressed
+### postcss版
 
-postcss dist/css/style.css \
-  --replace \
-  --use autoprefixer css-mqpacker \
-  --no-map
+postcss src/scss/**/[^_]* \
+  --dir dist/css \
+  --use \
+    postcss-import \
+    postcss-nesting \
+    postcss-simple-vars \
+    cssnano \
+    autoprefixer \
+    css-mqpacker \
+  --no-map \
+  --parser postcss-scss
+  # --syntax postcss-scss \
 
-# postcss版
+### node-sass版
 
-### バグ：拡張子が.scssのままコンパイルされてしまう
+# node-sass src/scss/style.scss \
+#   --output dist/css/ \
+#   --output-style compressed
 
-# postcss src/scss/**/[^_]* \
-#   --dir dist/css \
-#   --use \
-#     postcss-import \
-#     postcss-nesting \
-#     postcss-simple-vars \
-#     cssnano \
-#     autoprefixer \
-#     css-mqpacker \
-#   --no-map \
-#   --syntax postcss-scss
-
-# 複数のimport.scssがある前提で設計する。ページ別で別々のスタイルを適用したい場合のため。
-# 相当いろいろ試したが、node-sassで複数のinputを設定するのはできなさそう
-
-# postcssオンリーで実行しようとして現在断念中
-# cssnano postcss-importはnode-sass使うなら不要
-# first/conf/postcss.config.js も不要
-# style2.scss, _content2.scss も不要
-# stylelint --config conf/stylelintrc.js src/scss/*.scss && \
-
-# postcss src/scss/style.scss -d dist/css/ \
+# postcss dist/css/style.css \
+#   --replace \
 #   --use autoprefixer css-mqpacker \
 #   --no-map
