7ea0992a956a1d99f77d42c1a699cc8f9275cc39
TANAKA KANJI <tanaka-kan@gnavi.co.jp>
2018-12-25 15:46:11 +0900

    postcssコンパイルでnestが効かなくなったので、node-sassに戻した

--- bin/build_css.sh
+++ bin/build_css.sh
@@ -1,29 +1,30 @@
 echo "----- [build_css.sh] -----"
 
-stylelint --config conf/stylelintrc.js src/scss/**/*.css --syntax scss
+stylelint --config conf/stylelintrc.js src/scss/**/*.scss --syntax scss
 
 ### postcss版
 
-postcss src/scss/**/[^_]* \
-  --dir dist/css \
-  --use \
-    postcss-import \
-    postcss-nesting \
-    postcss-simple-vars \
-    cssnano \
-    autoprefixer \
-    css-mqpacker \
-  --no-map \
-  --parser postcss-scss
+# postcss src/scss/**/[^_]* \
+#   --dir dist/css \
+#   --use \
+#     postcss-import \
+#     postcss-nesting \
+#     postcss-simple-vars \
+#     cssnano \
+#     autoprefixer \
+#     css-mqpacker \
+#   --no-map \
+#   --parser postcss-scss
+
   # --syntax postcss-scss \
 
 ### node-sass版
 
-# node-sass src/scss/style.scss \
-#   --output dist/css/ \
-#   --output-style compressed
+node-sass src/scss/style.scss \
+  --output dist/css/ \
+  --output-style compressed
 
-# postcss dist/css/style.css \
-#   --replace \
-#   --use autoprefixer css-mqpacker \
-#   --no-map
+postcss dist/css/style.css \
+  --replace \
+  --use autoprefixer css-mqpacker \
+  --no-map
---
+++ conf/postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'autoprefixer': {},
+    // 'postcss-url': {},
+    // 'postcss-flexbugs-fixes': {},
+    // 'postcss-reporter': {},
+    'postcss-simple-vars': {},
+    'postcss-nested': {},
+    // 'postcss-extend': {},
+    // 'postcss-custom-properties': {}
+    'postcss-nesting': {},
+  }
+}
