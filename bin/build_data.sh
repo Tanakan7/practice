e7ad5e8c1f1575c9f1450e92d79f4d02aad28c02
TANAKA KANJI <tanaka-kan@gnavi.co.jp>
2019-03-06 18:20:57 +0900

    コマンドラインのエラー修正

--- bin/build_data.sh
+++ bin/build_data.sh
@@ -1,13 +1,16 @@
 echo "----- [build_data.sh] -----"
-echo $1
+doHtmlBuild="buildByWatch"
+
 # それらをdataプロパティの中に格納したbundle.jsonを生成
 jq -s add src/ejs/data/common/*.json src/ejs/data/module/*.json | \
   jq '{"data":.}' \
   > src/ejs/data/bundle.json
 
-# Bug: watch時だけ以下を実行したい(npm start時にも不要に実行されてしまう)
-if [ $1 = "buildByWatch" ] ; then
+# コマンドライン引数が無いとき変数を囲むダブルクォート無いとエラー出る(以下)
+if [ "$1" = "$doHtmlBuild" ]; then
   echo "--- < buildByWatch > ---"
   npm run build:html
   browser-sync reload
+else
+  echo "--- < normal > ---"
 fi
